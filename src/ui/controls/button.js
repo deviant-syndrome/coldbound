d00200cf43c32aa626193f5c55b9bd9455f7141d
deviant-syndrome <86923754+deviant-syndrome@users.noreply.github.com>
2022-02-12 17:12:11 +0200

    Added button press animation

--- src/ui/controls/button.js
+++ src/ui/controls/button.js
@@ -4,6 +4,7 @@ export function wire(domain, func, ctrl) {
   let keyCode = button(func);
   let buttonEl = domain.select(keyCode);
 
+  buttonEl.attr("class", "button");
   buttonEl.on("click", function () {
     ctrl(keyCode);
   });
